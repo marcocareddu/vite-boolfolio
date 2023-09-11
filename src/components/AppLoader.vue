4f5cb9418a3bff3fc25bb3bbb6f9b476083790d8
marcocareddu <saldumaccu@tuta.io>
2023-09-11 18:21:15 +0200

    Styled AppLoader

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<template>
+    <div class="d-flex justify-content-center align-items-center">
+        <h1>loading...</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+div {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(0.5, 0.5, 0.5, 0.8);
+}
+
+h1 {
+    color: #54A7D7;
+}
+</style>
