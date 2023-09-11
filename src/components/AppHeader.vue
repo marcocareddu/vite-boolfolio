70f3671d3355d8f3590616b2667a653b562d3f5e
marcocareddu <saldumaccu@tuta.io>
2023-09-11 14:52:55 +0200

    Added AppHeader base template

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,19 @@
 <script>
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Progetti</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
 
 <style scoped></style>
