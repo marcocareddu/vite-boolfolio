c8033739640e69093f20cd3e54bd9620754bef01
marcocareddu <saldumaccu@tuta.io>
2023-09-11 15:58:44 +0200

    Added API Call & diplay in page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,32 @@
 <script>
 import axios from 'axios';
+
+// Api Endpoints
+const baseEndpoint = 'http://127.0.0.1:8000/api/projects/'
+
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        fetchProjects() {
+            axios.get(baseEndpoint).then(res => { this.projects = res.data; })
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <div class="container">
+        <ul>
+            <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+        </ul>
+    </div>
+</template>
 
 <style></style>
