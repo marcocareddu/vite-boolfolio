02ae8bb21f73b07ecf59f6427322163ff6fb371f
marcocareddu <saldumaccu@tuta.io>
2023-09-11 15:20:33 +0200

    Replaced Header with boostrap Nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,17 +3,26 @@
 
 <template>
     <header>
-        <div class="container">
-            <ul class="nav nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Progetti</a>
-                </li>
-            </ul>
-        </div>
+
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body">
+            <div class="container">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarColor01">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Progetti</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </header>
 </template>
 
 <style scoped></style>
+
