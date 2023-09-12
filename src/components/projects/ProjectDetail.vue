ee3a3319206a37bb447f9d2ac56ffd253ae09505
marcocareddu <saldumaccu@tuta.io>
2023-09-12 16:00:39 +0200

    Detail page - Display API result in page

--- src/components/projects/ProjectDetail.vue
+++ src/components/projects/ProjectDetail.vue
@@ -1,13 +1,37 @@
 <script>
+import axios from 'axios';
+
+// Api Endpoints
+const baseEndpoint = 'http://127.0.0.1:8000/api/projects/'
 
 export default {
-    name: 'ProjectDetail'
+    name: 'ProjectDetail',
+    data() {
+        return {
+            project: [],
+            apiLoading: false
+        }
+    },
+    methods: {
+        getProject(baseEndpoint = 'http://127.0.0.1:8000/api/projects/') {
+            this.apiLoading = true;
+            axios.get(baseEndpoint + `${this.$route.params.id}`)
+                .then(res => {
+                    this.project = res.data;
+                })
+                .catch(err => { console.log(err) })
+                .then(() => { this.apiLoading = false })
+        }
+    },
+    created() {
+        this.getProject();
+    }
 };
 </script>
 
 <template>
     <div class="container">
-        <h1>PROVA DETTAGLIO</h1>
+        <h1>PROVA DETTAGLIO {{ project.name }}</h1>
     </div>
 </template>
 
