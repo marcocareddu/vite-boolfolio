1c7658d4741e273412ea9d7e8afcce603605f12d
marcocareddu <saldumaccu@tuta.io>
2023-09-12 14:37:48 +0200

    Index.js - Added 2 routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import AppMain from '../components/AppMain.vue';
+import ProjectDetail from '../components/ProjectDetail.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        { path: '/', name: 'home', component: AppMain },
+        { path: '/projects', name: 'projects', component: ProjectDetail }
     ]
 
 
