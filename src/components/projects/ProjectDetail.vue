e240f70a31c5efd56a547f73eb83fc38408c7ced
marcocareddu <saldumaccu@tuta.io>
2023-09-12 16:04:21 +0200

    Display Detail card

--- src/components/projects/ProjectDetail.vue
+++ src/components/projects/ProjectDetail.vue
@@ -1,11 +1,13 @@
 <script>
 import axios from 'axios';
+import AppCard from '../AppCard.vue';
 
 // Api Endpoints
 const baseEndpoint = 'http://127.0.0.1:8000/api/projects/'
 
 export default {
     name: 'ProjectDetail',
+    components: { AppCard },
     data() {
         return {
             project: [],
@@ -31,7 +33,9 @@ export default {
 
 <template>
     <div class="container">
-        <h1>PROVA DETTAGLIO {{ project.name }}</h1>
+        <h1>PROVA DETTAGLIO</h1>
+
+        <AppCard :data="project" />
     </div>
 </template>
 
