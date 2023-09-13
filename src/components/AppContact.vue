2e6d5b33f1aca6855c367da015f02832fc2e5605
marcocareddu <saldumaccu@tuta.io>
2023-09-13 14:36:31 +0200

    AppContact- Added base template

--- src/components/AppContact.vue
+++ src/components/AppContact.vue
@@ -1,3 +1,18 @@
 <script></script>
-<template></template>
+<template>
+    <div class="container d-flex justify-content-center">
+
+        <!-- Email Address Input -->
+        <div class="mb-3">
+            <label for="mail" class="form-label">Indirizzo email</label>
+            <input type="email" class="form-control" id="mail" placeholder="Inserisci la tua email">
+        </div>
+
+        <!-- Email Text Input -->
+        <div class="mb-3">
+            <label for="mail-text" class="form-label">Test email</label>
+            <textarea class="form-control" id="mail-text" rows="5"></textarea>
+        </div>
+    </div>
+</template>
 <style></style>
