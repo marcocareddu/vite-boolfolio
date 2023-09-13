4ef9ab0ca76bda8c6529154c7ecd89c24be96f12
marcocareddu <saldumaccu@tuta.io>
2023-09-13 14:38:23 +0200

    Added AppContact route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from '../components/AppHome.vue';
+import AppContact from '../components/AppContact.vue';
 import ProjectDetail from '../components/projects/ProjectDetail.vue';
 import ProjectsList from '../components/projects/ProjectsList.vue';
 
@@ -7,11 +8,10 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'home', component: AppHome },
+        { path: '/contact', name: 'contact', component: AppContact }
         { path: '/projects', name: 'projects', component: ProjectsList },
-        { path: '/projects/:id', name: 'project-detail', component: ProjectDetail }
+        { path: '/projects/:id', name: 'project-detail', component: ProjectDetail },
     ]
-
-
 })
 
 export { router };
