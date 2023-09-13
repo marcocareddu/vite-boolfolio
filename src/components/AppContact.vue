3613815ef5bb3823f2cb3450709cb6355b4239a6
marcocareddu <saldumaccu@tuta.io>
2023-09-13 14:43:21 +0200

    AppContact - Fixed style, added form w/ button

--- src/components/AppContact.vue
+++ src/components/AppContact.vue
@@ -1,18 +1,23 @@
 <script></script>
 <template>
-    <div class="container d-flex justify-content-center">
+    <div class="container mt-5">
 
-        <!-- Email Address Input -->
-        <div class="mb-3">
-            <label for="mail" class="form-label">Indirizzo email</label>
-            <input type="email" class="form-control" id="mail" placeholder="Inserisci la tua email">
-        </div>
+        <form>
+            <!-- Email Address Input -->
+            <div class="mb-5">
+                <label for="mail" class="form-label">Indirizzo email</label>
+                <input type="email" class="form-control" id="mail" placeholder="Inserisci la tua email">
+            </div>
 
-        <!-- Email Text Input -->
-        <div class="mb-3">
-            <label for="mail-text" class="form-label">Test email</label>
-            <textarea class="form-control" id="mail-text" rows="5"></textarea>
-        </div>
+            <!-- Email Text Input -->
+            <div class="mb-3">
+                <label for="mail-text" class="form-label">Testo email</label>
+                <textarea class="form-control" id="mail-text" rows="5"></textarea>
+            </div>
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-light">Invia</button>
+            </div>
+        </form>
     </div>
 </template>
 <style></style>
